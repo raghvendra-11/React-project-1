644ed01522dcccc09a0389b5d59e987b88e6f558
cse-raghvendra <60070962+cse-raghvendra@users.noreply.github.com>
2024-05-07 13:14:28 +0530

    first commit

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,15 @@
+function App(){
+    return (
+        <div>
+            <nav>
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="logo" />
+
+
+            </div>
+            </nav>
+        </div>
+    )
+}
+
+export default App;
